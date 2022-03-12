1bfba9e95bbf8de34d3912569cfd0e2b230b5863
Gabriel Alves da silva <gabrielalvesdasilva12452@gmail.com>
2022-03-12 18:08:16 -0300

    mo

--- exercicios/ex006.py
+++ exercicios/ex006.py
@@ -1,3 +1,4 @@
+#dobro triplo e raiz quadrada
 n1 = int(input('Digite um numero para ver o seu dobro, triplo e, e raiz quadrada:'))
-print('O dobro de {} é {} o triplo é {} e a Raiz quadrada é {}'.format(n1, n1+n1, n1*3, n1**(1/2) ))
+print('O dobro de {} é {}. o triplo é {}. e \na Raiz quadrada é {:.2f}.'.format(n1, n1+n1, n1*3, n1**(1/2) ))
 
