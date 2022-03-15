db36a0d1288a484cd83366e4467b564d796e62fd
Gabriel Alves da silva <gabrielalvesdasilva12452@gmail.com>
2022-03-15 17:33:37 -0300

    ex021 atualizado

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,5 +1,5 @@
-import pygame
-pygame.init()
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+from pygame import mixer
+mixer.init()
+mixer.music.load('ex021.mp3')
+mixer.music.play()
+input('Agora voce escuta?')
